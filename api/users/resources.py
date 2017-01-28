1e640d4381a2cee97e6bd9c679dc0c63d2cd5576
Collin Mutembei <mutembei.collin@gmail.com>
2017-01-28 23:38:41 +0300

    :incoming_envelope: Accept logins
    - WIP

--- api/users/resources.py
+++ api/users/resources.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource, fields, marshal
+from flask_restful import Resource, fields, marshal_with
 from flask_restful.reqparse import RequestParser
 
 from api.app import db
@@ -13,7 +13,9 @@ class SignUp(Resource):
     """creates a user:
             POST {"username": "patientzer0", "password": "YmlnZ2VzdGJlaGluZA"}
     """
+    @marshal_with(user_fields)
     def post(self):
+
         parser = RequestParser()
         parser.add_argument('username', type=str, required=True)
         parser.add_argument('password', type=str, required=True)
@@ -24,4 +26,22 @@ class SignUp(Resource):
         signup_user.set_password(args.password)
         db.session.add(signup_user)
         db.session.commit()
-        return marshal(signup_user, user_fields)
+
+
+class SignIn(Resource):
+    """creates a user:
+            POST {"username": "patientzer0", "password": "YmlnZ2VzdGJlaGluZA"}
+    """
+    @marshal_with(user_fields)
+    def post(self):
+
+        parser = RequestParser()
+        parser.add_argument('username', type=str, required=True)
+        parser.add_argument('password', type=str, required=True)
+        args = parser.parse_args()
+        signup_user = User(
+            username=args.username,
+        )
+        signup_user.set_password(args.password)
+        db.session.add(signup_user)
+        db.session.commit()
