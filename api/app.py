f71c8e0882a8658be5f7f1380c93fa0e1ed1ddfa
Collin Mutembei <mutembei.collin@gmail.com>
2017-01-28 22:52:07 +0300

    :house_with_garden: house keeping
    - move around imports to resolve cyclic imports
    - refactor tests to stop tearing apart db when run

--- api/app.py
+++ api/app.py
@@ -3,18 +3,24 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_restful import Api
 
 from api.config import config
 
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
+api = Api(app)
 bcrypt = Bcrypt(app)
+db = SQLAlchemy(app)
 
+# import in the middle of code to avoid cyclic imports
+from api.users.models import User
 
 # Set the config for app
 config_name = os.environ.get('APP_SETTINGS', 'default')
 app.config.from_object(config[config_name])
 
 # import in the middle of code to avoid cyclic imports
-from api.users.models import User
+from api.users.resources import SignUp
+
+api.add_resource(SignUp, '/signup/')
