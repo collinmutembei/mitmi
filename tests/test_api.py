411c3e0a202d8fc50489caf870961d98625f4fc4
Collin Mutembei <mutembei.collin@gmail.com>
2017-01-28 08:49:14 +0300

    :wavy_dash: Test signup and signin

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,13 +1,62 @@
 import unittest
 
+import unirest
+
 
 class MITMITestCase(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.server_url = 'http://mitmi.herokuapp.com/api'
+        self.test_username = "patientzer0"
+        self.test_password = "YmlnZ2VzdGJlaGluZA"
 
-    def test_basic_test(self):
-        self.assertEqual(1, 1)
+    def test_get_method_not_allowed_on_signup(self):
+        """ assert that GET request are not allowed on signup resource
+        """
+        signup_get = unirest.get(
+            "{0}/signup/".format(self.server_url),
+            headers={"Accept": "application/json"},
+        )
+        self.assertEqual(signup_get.code, 405)
+
+    def test_creating_new_user(self):
+        """ assert that POST request on signup resource creates new user
+        """
+        signup_get = unirest.post(
+            "{0}/signup/".format(self.server_url),
+            headers={"Accept": "application/json"},
+            params={
+                "username": self.test_username,
+                "password": self.test_password
+            }
+        )
+        self.assertEqual(signup_get.code, 200)
+
+    def test_getting_token_for_existing_user(self):
+        """ assert that GET request are not allowed on signup resource
+        """
+        signup_get = unirest.post(
+            "{0}/signin/".format(self.server_url),
+            headers={"Accept": "application/json"},
+            params={
+                "username": self.test_username,
+                "password": self.test_password
+            }
+        )
+        self.assertEqual(signup_get.code, 200)
+
+    def test_getting_token_for_non_existing_user(self):
+        """ assert that GET request are not allowed on signup resource
+        """
+        signup_get = unirest.post(
+            "{0}/signin/".format(self.server_url),
+            headers={"Accept": "application/json"},
+            params={
+                "username": "user",
+                "password": "password"
+            }
+        )
+        self.assertEqual(signup_get.code, 401)
 
     def tearDown(self):
         pass
