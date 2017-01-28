998260a6cdc75b2a9f450b0ab4cc7f495e36a33e
Collin Mutembei <mutembei.collin@gmail.com>
2017-01-28 16:02:16 +0300

    :dvd: Add db command
    - Use flask-script to manage database and migrations
    - Pass models to db context for access from shell command

--- manage.py
+++ manage.py
@@ -1,10 +1,17 @@
 import unittest
 
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
-from api.app import app
+from api.app import app, db
+from api.users.models import User
+from api.events.models import Event
 
 manager = Manager(app)
+migrate = Migrate(app, db)
+
+# db command
+manager.add_command('db', MigrateCommand)
 
 
 # custom script command to run unit tests
@@ -14,6 +21,11 @@ def test():
     test_runner = unittest.TextTestRunner(verbosity=2)
     test_suite = test_loader.discover('tests')
     test_runner.run(test_suite)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Event=Event)
 
 
 if __name__ == "__main__":
