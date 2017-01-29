ac80c6771237d64b48440683e7664a0079376c11
Collin Mutembei <mutembei.collin@gmail.com>
2017-01-29 15:16:27 +0300

    :space_invader: Refactor resources
    - tweak the signup and signin resources
    - a couple more tests
    - use faker

--- api/users/resources.py
+++ api/users/resources.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource, fields, marshal_with
+from flask_restful import Resource, fields, marshal
 from flask_restful.reqparse import RequestParser
 
 from api.app import db
@@ -13,35 +13,43 @@ class SignUp(Resource):
     """creates a user:
             POST {"username": "patientzer0", "password": "YmlnZ2VzdGJlaGluZA"}
     """
-    @marshal_with(user_fields)
     def post(self):
 
         parser = RequestParser()
         parser.add_argument('username', type=str, required=True)
         parser.add_argument('password', type=str, required=True)
         args = parser.parse_args()
+
+        signup_user = User.query.filter_by(username=args.username).first()
+        if signup_user:
+            return {'message': 'user with that name already exists'}, 400
+
         signup_user = User(
             username=args.username,
         )
         signup_user.set_password(args.password)
         db.session.add(signup_user)
         db.session.commit()
+        return marshal(signup_user, user_fields), 201
 
 
 class SignIn(Resource):
     """creates a user:
             POST {"username": "patientzer0", "password": "YmlnZ2VzdGJlaGluZA"}
     """
-    @marshal_with(user_fields)
     def post(self):
 
         parser = RequestParser()
         parser.add_argument('username', type=str, required=True)
         parser.add_argument('password', type=str, required=True)
         args = parser.parse_args()
-        signup_user = User(
-            username=args.username,
-        )
-        signup_user.set_password(args.password)
-        db.session.add(signup_user)
-        db.session.commit()
+
+        signin_user = User.query.filter_by(username=args.username).first()
+        # if user exists
+        if signin_user:
+            # check password
+            if signin_user.verify_password(args.password):
+                return {'token': 'something'}, 200
+            # password not correct
+            return {'message': 'password did not work'}, 403
+        return {'message': 'invalid user'}, 401
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,10 @@
 import unittest
 
+from faker import Faker
+
 from api.app import app, db
 from api.config import config
+from api.users.models import User
 
 
 class MITMITestCase(unittest.TestCase):
@@ -9,10 +12,20 @@ class MITMITestCase(unittest.TestCase):
     def setUp(self):
         app.config.from_object(config['testing'])
         self.client = app.test_client()
+
+        self.fake = Faker()
+
         db.create_all()
 
         self.test_username = "patientzer0"
         self.test_password = "YmlnZ2VzdGJlaGluZA"
+
+        test_user = User(
+            username=self.test_username,
+            password=self.test_password
+        )
+        db.session.add(test_user)
+        db.session.commit()
 
     def test_get_method_not_allowed_on_signup(self):
         """ assert that GET request are not allowed on signup resource
@@ -30,11 +43,47 @@ class MITMITestCase(unittest.TestCase):
             "/signup/",
             headers={"Accept": "application/json"},
             data={
-                "username": self.test_username,
-                "password": self.test_password
+                "username": self.fake.user_name(),
+                "password": self.fake.password()
             }
         )
-        self.assertEqual(signup_get.status_code, 200)
+        self.assertEqual(signup_get.status_code, 201)
+
+    def test_creating_new_user_with_username_of_existing_user(self):
+        """ assert that POST request on signup resource with username
+        matching an already existing user
+        """
+        signup_get = self.client.post(
+            "/signup/",
+            headers={"Accept": "application/json"},
+            data={
+                "username": "patientzer0",
+                "password": self.fake.password()
+            }
+        )
+        self.assertEqual(signup_get.status_code, 400)
+
+    def test_creating_new_user_with_missing_field(self):
+        """ assert that POST request on signup resource fails if required
+        field is missing
+        """
+        signup_get = self.client.post(
+            "/signup/",
+            headers={"Accept": "application/json"},
+            data={
+                "username": self.fake.user_name(),
+            }
+        )
+        self.assertEqual(signup_get.status_code, 400)
+
+    def test_get_method_not_allowed_on_signin(self):
+        """ assert that GET request are not allowed on signin resource
+        """
+        signup_get = self.client.get(
+            "/signin/",
+            headers={"Accept": "application/json"},
+        )
+        self.assertEqual(signup_get.status_code, 405)
 
     def test_getting_token_for_existing_user(self):
         """ assert that signin for valid user gets token
@@ -48,6 +97,19 @@ class MITMITestCase(unittest.TestCase):
             }
         )
         self.assertEqual(signup_get.status_code, 200)
+
+    def test_getting_token_for_existing_user_with_wrong_password(self):
+        """ assert that signin for valid user with wrong password
+        """
+        signup_get = self.client.post(
+            "/signin/",
+            headers={"Accept": "application/json"},
+            data={
+                "username": self.test_username,
+                "password": self.fake.password()
+            }
+        )
+        self.assertEqual(signup_get.status_code, 403)
 
     def test_getting_token_for_non_existing_user(self):
         """ assert that signin for invalid user fails
