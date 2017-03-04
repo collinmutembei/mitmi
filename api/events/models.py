6e9406feb5ef5e3c7c4291ad9fcc28b3cdb2226f
Collin Mutembei <mutembei.collin@gmail.com>
2017-03-04 17:11:13 +0300

    :busts_in_silhouette: Everyone is invited
    - Remove field to track maximum guests at an event
    - Generate migrations

--- api/events/models.py
+++ api/events/models.py
@@ -14,26 +14,14 @@ class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
     venue = db.Column(db.String(80))
-    max_guests = db.Column(db.String(80))
     guest = db.relationship(
         'User',
         secondary=guests,
         backref=db.backref('event', lazy='dynamic')
     )
 
-    def is_full(self, plaintext):
-        """Returns true if event has reached max capacity of guests
-        """
-        pass
-
-    def remaining_slots(self, plaintext):
-        """Returns number of remaining guest slots till event is full
-        """
-        pass
-
     def __repr__(self):
-        return '<Event {0} happening at {1} : {2} Slots remaining>'.format(
+        return '<Event {0} happening at {1}>'.format(
             self.name,
-            self.venue,
-            self.remaining
+            self.venue
         )
